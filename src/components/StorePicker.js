7c27dc989698cee259be2cebf915c4652affaec0
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-10-07 00:04:21 +0530

    first build (test) :tada:

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class CafePicker extends React.Component {
+
+    render () {
+        return <p>Cafe Picker Component rendered!</p>
+    }
+}
+
+export default CafePicker;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import CafePicker from "./components/StorePicker";
+
+// This is another way of importing just the Component from the react module
+// import { Component } from 'react';
+
+
+
+render(<CafePicker />, document.querySelector('#main'));
