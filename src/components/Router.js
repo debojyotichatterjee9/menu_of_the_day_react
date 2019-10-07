dad6b142cfd48270672a46e79ebcf3c11ac75895
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-10-07 22:01:20 +0530

    added the not found page and fixed routing errors

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CafePicker from "./CafePicker";
+import App from "./App";
+import NotFound from "./NotFound";
 
 const Router = () => (
 
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={CafePicker}/>
+            <Route path="/cafe/:cafeId" component={App} />
+            <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
 );
