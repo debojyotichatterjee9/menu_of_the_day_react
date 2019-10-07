592540a7f57576d8c06faf2ae0e6614517316121
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-10-07 18:15:31 +0530

    changed the header component to stateless functional component :zap: and fixed minor typos :pencil:

--- src/components/CafePicker.js
+++ src/components/CafePicker.js
@@ -4,14 +4,14 @@ class CafePicker extends React.Component {
 
     render() {
 
-        /*So, if we want to return some multiline html using JSX we cannot just leave the return statement blank and
+        /* So, if we want to return some multiline html using JSX we cannot just leave the return statement blank and
         start from the next line. Javascipt has a feature called ASI (Automatic Semicolon Insertion), which will
         inser the semicolon automatically and eventually return nothing so we use the parenthesis "( )" 
         to make it work and easier to read as well. */
 
-        /*Note: You cannot return a sibling element here just one element it may be nested, that'll work just fine.
+        /* Note: You cannot return a sibling element here just one element it may be nested, that'll work just fine.
         Latest update of React has a feature where you can wrap the whole thing into <React.Fragment></React.Fragment>
-        tag or just empty tag like this --> <></> and you can render sibling elements in that.*/
+        tag or just empty tag like this --> <></> and you can render sibling elements in that. */
         return (
             <>
             <form action="" className="cafe-selector">
@@ -24,7 +24,7 @@ class CafePicker extends React.Component {
         );   
 
         /* below is another way of rendering element in DOM
-        this is a nested element which has --> <div class="heading sample"><h3 class="actualHeading">Test heading.</h3></div>*/
+        this is a nested element which has --> <div class="heading sample"><h3 class="actualHeading">Test heading.</h3></div> */
         // return React.createElement('div', { className: 'heading sample'},React.createElement('h3', { className: 'actualHeading'}, 'Test heading.'));
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,54 @@
 import React from "react";
 
-class Header extends React.Component {
-    render () {
-        return (
-            <>
-            <header className="top">
-                <h1>Menu
-                    <span className="ofThe">
-                        <span className="of">
-                        of
-                        </span>
-                        <span className="the">
-                        the
-                        </span>
-                    </span>
-                    Day</h1>
-            <h3 className="tagline">
-                <span>{this.props.tagline}</span>
+
+/* Note: The Header component below just has one render function and does not have any dynaminc data in it
+the data is fed to it, it just resnders some HTML in the DOM. Hence, there is no need for it to be a full 
+blown React Component. So, here we can use a stateless functional component. If a component only has a render
+method and prop types then you can convert the React Component into a stateless functional component, it is
+unecessary to write a React Component for it. This practice can avoid verbose and enhance performance as well. */
+
+const Header = ({ tagline }) => ( // here ES6 destructuring is used, "tagline" is accessing "props.tagline" directly
+    <>
+        <header className="top">
+            <h1>
+                Menu
+        <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day
+            </h1>
+                <h3 className="tagline">
+                    <span>{tagline}</span>
             </h3>
-            </header>
-            </>
-        );
-    }
-}
+        </header>
+    </>
+);
+
+
+/*We may use this if required otherwise for only rendering HTML in the DOM, above stateless functional component is a better way. */
+// class Header extends React.Component {
+//     render () {
+//         return (
+//             <>
+//             <header className="top">
+//                 <h1>Menu
+//                     <span className="ofThe">
+//                         <span className="of">
+//                         of
+//                         </span>
+//                         <span className="the">
+//                         the
+//                         </span>
+//                     </span>
+//                     Day</h1>
+//             <h3 className="tagline">
+//                 <span>{this.props.tagline}</span>
+//             </h3>
+//             </header>
+//             </>
+//         );
+//     }
+// }
 
 export default Header;
