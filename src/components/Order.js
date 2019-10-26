4b9bdbfe8a60014423cda62b43ccc30f63345200
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-10-26 20:12:46 +0530

    updated the order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,16 +2,23 @@ import React from "react";
 import { formatPrice } from "../helpers"
 
 class Order extends React.Component {
+    
     renderOrder = key => {
         const item = this.props.items[key];
         const count = this.props.order[key];
-        return <li>
-            {count} x {item.name}
 
-            {formatPrice(count * item.price)}
-        </li>
+        if (!item || item.status === 'unavailable') {
+            return <li key={key}>Sorry, {item ? item.name : 'item'} is no longer available.</li>
+        }
+
+        return (
+            <li key={key}>
+            <span>{count} ✖ {item.name} 👉 </span>    
+                <span className="price">{formatPrice(count * item.price)}</span>   
+            </li> 
+        )
     }
-    render () {
+    render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
             const item = this.props.items[key];
@@ -27,14 +34,14 @@ class Order extends React.Component {
             <>
                 <div className="order-wrap">
                     <h2>Orders</h2>
-                    <ul>
-                    {orderIds.map(this.renderOrder)}
+                    <ul className="order">
+                        {orderIds.map(this.renderOrder)}
                     </ul>
-                        <div className="total">
-                            Total:<strong>
-                                {formatPrice(total)}
-                            </strong>
-                            </div>
+                    <div className="total">
+                        Total:<strong>
+                            {formatPrice(total)}
+                        </strong>
+                    </div>
                 </div>
             </>
         );
