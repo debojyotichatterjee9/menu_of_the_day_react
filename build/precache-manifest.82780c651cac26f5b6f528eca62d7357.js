85dc047e6f2a7031798674274eafe893c7af3a05
Debojyoti Chatterjee <debojyoti@glionconsulting.com>
2019-11-03 22:46:13 +0530

    deployed on now.sh

---
+++ build/precache-manifest.82780c651cac26f5b6f528eca62d7357.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "09df72c47cfe29e45cf2",
+    "url": "/static/css/main.cb09d6ef.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ee25e35e8a84b03af0b",
+    "url": "/static/js/2.941fd8bf.chunk.js"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "863f3103d7aea5816e2e5812519af4b0",
+    "url": "/static/media/chef.863f3103.svg"
+  },
+  {
+    "revision": "09df72c47cfe29e45cf2",
+    "url": "/static/js/main.b045acc1.chunk.js"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "fc2ec55e79ae9e12b0edbe73d693739f",
+    "url": "/index.html"
+  }
+];
